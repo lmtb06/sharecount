c02ace5f8dfcf86d61d5200915b7d0dab7c96b8f
Elhadji Moussa FAYE <moussafaye.m@gmail.com>
2021-11-13 19:54:33 +0100

    -Modification diagramme de classe -Ajout de fonctions et test de la classe CompteUtilisateur

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
 #include "CompteUtilisateur.hpp"
 
+/**
+ * @brief Test de la classe CompteUtilisateur
+ *
+ * @version 0.1
+ *
+ * @author Elhadji Moussa FAYE
+ */
+
 int main()
 {
-	CompteUtilisateur c = CompteUtilisateur("Bla", "Mon prenom", "Mon nom", "Blabla", "Blal");
-	std::cout << "Hello " << c.getNom() << " !" << std::endl;
+	CompteUtilisateur c = CompteUtilisateur("lmtb", "Elhadji Moussa", "FAYE", "moussafaye.m@gmail.com", "SHA256-HASH");
+	// Test des getters
+	std::cout << "Pseudo : " << c.getPseudo() << std::endl
+						<< "Prenom : " << c.getPrenom() << std::endl
+						<< "Nom : " << c.getNom() << std::endl
+						<< "Email : " << c.getEmail() << std::endl
+						<< "Hash mot de passe : " << c.getHashMotDePasse() << std::endl;
+	// Test des setteurs
+	std::cout << "=======Modification infos comptes=======" << std::endl;
+	c.setPseudo("lmtb06");
+	c.setPrenom("Moussa");
+	c.setNom("FAYE");
+	c.setEmail("moussafaye.m@gmail.com");
+	std::cout << "Pseudo : " << c.getPseudo() << std::endl
+						<< "Prenom : " << c.getPrenom() << std::endl
+						<< "Nom : " << c.getNom() << std::endl
+						<< "Email : " << c.getEmail() << std::endl
+						<< "Hash mot de passe : " << c.getHashMotDePasse() << std::endl;
+
 	return 0;
 }
